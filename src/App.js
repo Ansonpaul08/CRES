815e580335d8f2443f331a611b1ad55bc4aa3694
Ansonpaul08 <ansonpaul48@gmail.com>
2021-11-18 19:07:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="main">
+      <label><b>Welcome To CRES</b></label> 
+        <div class="admin">
+            <h1>ADMIN LOGIN</h1>
+            <label id="username"class="label">Username</label>
+            <input type="text" class="form1"></input> 
+            <label id="password"class="label">Password</label>
+            <input type="password" class="form"></input>
+            <button id="button">LOGIN</button>
+         </div>
+         <div>
+            
+            
+          </div>   
+    </div>
+   
+  );
+}
+
+export default App;
